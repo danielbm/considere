65227ca6afaa2fa8fcd9b737872be9514cb9691f
Daniel <danielbm@users.noreply.github.com>
2023-07-19 14:49:54 -0300

    bump jun 23

--- app/build/service-worker.js
+++ app/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e1f8bd00c5ea5190f6133a5d02575462.js"
+  "/precache-manifest.d369787baa1e39e1f5840493f92f0638.js"
 );
 
 self.addEventListener('message', (event) => {
